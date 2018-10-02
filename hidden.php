287f35d81f6eedca58b794323b426388c8044a66
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-10-02 10:41:14 +0200

    Add files via upload

---
+++ hidden.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+    $nombres = array();
+    if (isset($_POST['enviar'])) {
+        if (isset($_POST['nombres'])) {
+			$_POST['nombres']=str_replace("  "," ",$_POST['nombres']);
+		
+		    $nombres = $_POST['nombres'];
+        }
+		$_POST['nombre']=trim($_POST['nombre']);
+		if($_POST['nombre']!=""){
+
+        $nombres[] = $_POST['nombre'];
+		}
+    }
+
+    if (!empty($nombres)) {
+
+        echo '<p>Lista de nombres:</p><ul>';
+        foreach($nombres as $n) {
+            echo '<li>' . $n . '</li>';
+        }
+        echo '</ul>';
+    }
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+Nombre: <input type="text" name="nombre" />
+<?php
+    if (!empty($nombres)) {
+
+        foreach($nombres as $n) {
+            echo '<input type="hidden" name="nombres[]" value="' . $n . '" />';
+        }
+    }
+?>
+<input type="submit" name="enviar" value="enviar" />
+</form>
+</body>
+</html>
