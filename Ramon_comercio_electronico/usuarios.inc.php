5b3944f13e2ff6a980c1f2e3a03a600fd486e111
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-10-11 14:34:49 +0200

    Add files via upload

---
+++ Ramon_comercio_electronico/usuarios.inc.php
@@ -0,0 +1,11 @@
+<?php
+    $users = array();
+    $users[] = array('id' => "0", 
+                     'username' => "manolo92",
+                     'nombre' => "Manuel Molina",
+                     'password' => 'ba5feb35eabd76f18fbe4b45531bc2ab'); // sevilla
+    $users[] = array('id' => "1", 
+                     'username' => "monikka",
+                     'nombre' => "Monica Gonzalez",
+                     'password' => '0c671066ca88628d9388b41735e891b3'); // 14022008
+?>
