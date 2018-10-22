e4519bf0e50963d0141f0cfd501994ddb003c017
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-10-22 13:35:13 +0200

    Update importar_BD.php

--- BD/importar_BD.php
+++ BD/importar_BD.php
@@ -6,13 +6,13 @@ $host="localhost";
 $bd="prueba";
 $nombre="backup.sql"; 
 
-$conecta = mysql_connect($host, $usuario, $passwd); 
+$conecta = mysqli_connect($host, $usuario, $passwd); 
 
 if (!$conecta) { 
 die('Error : ' . mysql_error()); 
 } 
 
-$db_selected = mysql_select_db($bd, $conecta); 
+$db_selected = mysqli_select_db($bd, $conecta); 
 
 if (!$db_selected) { 
 echo 'Fallo en la base de datos indicada',$db_selected,'<br/>'; 
@@ -24,12 +24,16 @@ else {
 
 $texto = file_get_contents($nombre);
 $sentencia = explode(";", $texto);
+			
+            for ($i = 0; $i < (count($sentencia) - 1); $i++) {
+                $sentencia[$i] .= ";";
+                mysqli_query($conexion, $sentencia[$i]);
+  
+                //    mysqli_query ($conexion,"INSERT INTO JUGADORES VALUES(0," . $_POST['adminuser'] . " " . $_POST['passuser'] . " );";
+            }
 
-for($i = 0; $i < (count($sentencia)-1); $i++){
-$db_selected = mysql_query ("$sentencia[$i];");
+            mysqli_close($conexion);
  
-}
-
 print "La importación a tenido exito!";
 
 } 
