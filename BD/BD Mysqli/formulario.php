db23096e170d57ca8e8660a3d59c203784140b8c
ComandPromt <vayase_a_ferrol@hotmail.com>
2018-10-22 11:02:10 +0200

    Update formulario.php

--- BD/BD Mysqli/formulario.php
+++ BD/BD Mysqli/formulario.php
@@ -9,18 +9,21 @@
 <body>
 <?php
 // Launch http://localhost/formulario.php?product=3DSNG
-error_reporting(0);
+//error_reporting(0);
 $_GET["product"];
 
 $conexion = mysqli_connect("localhost", "dwes", "abc123", "dwes");
-$result = mysqli_query($conexion, "SELECT nombre_corto,descripcion,T.nombre AS 'Tienda',S.unidades FROM producto P JOIN stock S ON P.cod=S.producto JOIN tienda T ON S.tienda=T.cod
- WHERE P.cod='" . $_GET["product"] . "'");
+$result = mysqli_query($conexion, "
+SELECT nombre_corto,descripcion,T.nombre AS 'Tienda',S.unidades FROM producto P JOIN stock S ON P.cod=S.producto JOIN tienda T ON S.tienda=T.cod
+WHERE P.cod='" . $_GET["product"] . "'");
 
 $tienda = array();
 $x = 0;
-if ($fila = mysqli_fetch_array($result)) {
+
     print "<table style=\"text-align:center;\" border=1><tr><th>Nombre</th><th>Descripcion</th></tr>";
     while ($fila = mysqli_fetch_array($result)) {
+
+
         if ($x == 0) {
             print "<tr><td>" . $fila[0] . "</td><td>" . $fila[1] . "</td></td></tr>";
             $x=1;
@@ -34,11 +37,6 @@ if ($fila = mysqli_fetch_array($result)) {
         $x++;
     }
     print "</table>";
-} else {
-    print "<h1 style=\"color:red;\">No existe el producto con la id " . $_GET["product"] . "</h1>";
-
-}
-
 ?>
 </body>
 </html>
